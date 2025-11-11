a7cfc17ff0cd210ed11bc5568aa8e657dc092724
kirti123-code <knaik@knowledgehomeitsolutions.com>
2025-11-11 23:48:06 +0530

    Add files via upload

---
+++ CustomException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SERVICES.Common
+{
+    class CustomException : Exception
+    {
+        public string ErrorCode { get; }
+        public int StatusCode { get; }  // Optional if you want to map to HTTP status codes
+
+        public CustomException(string message, string errorCode = "UNKNOWN_ERROR", int statusCode = 400)
+            : base(message)
+        {
+            ErrorCode = errorCode;
+            StatusCode = statusCode;
+        }
+    }
+}
---
+++ ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using SERVICES.Interfaces;
+using SERVICES.Repository;
+using GymCraftSolutionsWebAPI.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SERVICES.Common
+{
+    public static class ServiceRegistration
+    {
+        public static void AddScopedServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            //services.AddScoped<ILoginService, LoginService>();
+            services.AddScoped<IUserDetailsService, UserDetailsService>();
+            services.AddScoped<IRolesService, RoleService>();
+            services.AddScoped<BusBookingService, BusBookingService>();
+        }
+    }
+}
