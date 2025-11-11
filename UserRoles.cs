6132e2bd6a1c31f603f5ab2408b7122c43c24a3b
kirti123-code <knaik@knowledgehomeitsolutions.com>
2025-11-11 23:42:45 +0530

    Add files via upload

---
+++ UserRoles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDCraftSolutionWebAPI.Models.Enums
+{
+    public enum UserRoles
+    {
+        Admin = 1,
+        User = 2,
+        SuprAdmin = 3,
+    }
+}
